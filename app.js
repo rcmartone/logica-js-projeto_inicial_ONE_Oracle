4a02af9ba2e9335e3739227bda04b38ce2cdb524
Rafael Martone <rafaelcmartone@gmail.com>
2025-01-15 12:58:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 5;
+let numeroEscolhido = prompt("Escolha um número entre 1 e 10");
+
+if (numeroSecreto == numeroEscolhido) {
+    console.log("Isso ai!! Você descobriu 
+        
+        
+        o numero secreto")
+}
